46cde6ed2531890dcc4a2d8423a7990fe2611227
Furkan Güzelgörür <furkanguzelgorur@gmail.com>
2024-04-27 17:39:28 +0300

    panel installed

---
+++ resources/views/panel/layout/app.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Purple Admin</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="{{asset("panel/assets/vendors/mdi/css/materialdesignicons.min.css")}}">
+    <link rel="stylesheet" href="{{asset("panel/assets/vendors/ti-icons/css/themify-icons.css")}}">
+    <link rel="stylesheet" href="{{asset("panel/assets/vendors/css/vendor.bundle.base.css")}}">
+    <link rel="stylesheet" href="{{asset("panel/assets/vendors/font-awesome/css/font-awesome.min.css")}}">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <link rel="stylesheet" href="{{asset("panel/assets/vendors/font-awesome/css/font-awesome.min.css")}}"/>
+    <link rel="stylesheet" href="{{asset("panel/assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css")}}">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="{{asset("panel/assets/css/style.css")}}">
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="{{asset("panel/assets/images/favicon.png")}}"/>
+</head>
+<body>
+<div class="container-scroller">
+    <!-- partial:partials/_navbar.html -->
+    @include("panel.inc.navbar")
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+        <!-- partial:partials/_sidebar.html -->
+        @include("panel.inc.sidebar")
+        <!-- partial -->
+        <div class="main-panel">
+            <div class="content-wrapper">
+                @yield('content')
+            </div>
+            <!-- content-wrapper ends -->
+            <!-- partial:partials/_footer.html -->
+            @include("panel.inc.footer")
+            <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+</div>
+<!-- container-scroller -->
+<!-- plugins:js -->
+<script src="{{asset("panel/assets/vendors/js/vendor.bundle.base.js")}}"></script>
+<!-- endinject -->
+<!-- Plugin js for this page -->
+<script src="{{asset("panel/assets/vendors/chart.js/chart.umd.js")}}"></script>
+<script src="{{asset("panel/assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js")}}"></script>
+<!-- End plugin js for this page -->
+<!-- inject:js -->
+<script src="{{asset("panel/assets/js/off-canvas.js")}}"></script>
+<script src="{{asset("panel/assets/js/misc.js")}}"></script>
+<script src="{{asset("panel/assets/js/settings.js")}}"></script>
+<script src="{{asset("panel/assets/js/todolist.js")}}"></script>
+<script src="{{asset("panel/assets/js/jquery.cookie.js")}}"></script>
+<!-- endinject -->
+<!-- Custom js for this page -->
+<script src="{{asset("panel/assets/js/dashboard.js")}}"></script>
+<!-- End custom js for this page -->
+</body>
+</html>
