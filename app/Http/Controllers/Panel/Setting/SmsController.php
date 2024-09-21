d0795974e7550a0b693f3db7facd307b6c7fc9d8
Furkan Güzelgörür <furkanguzelgorur@gmail.com>
2024-09-21 16:43:10 +0300

    Sms configuration update

--- app/Http/Controllers/Panel/Setting/SmsController.php
+++ app/Http/Controllers/Panel/Setting/SmsController.php
@@ -19,14 +19,6 @@ class SmsController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request): RedirectResponse
@@ -38,39 +30,6 @@ class SmsController extends Controller
             $model::set($key, [$value]);
         }
 
-
         return redirect()->back()->with("success", "Güncelleme Başarılı");
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
