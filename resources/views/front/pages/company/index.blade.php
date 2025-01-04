715633ba2934d4301f23800fb3f2209a0f17684b
Furkan Güzelgörür <furkanguzelgorur@gmail.com>
2025-01-05 01:04:36 +0300

    debug fix 2

--- resources/views/front/pages/company/index.blade.php
+++ resources/views/front/pages/company/index.blade.php
@@ -10,123 +10,126 @@
                         <div class="col-xxl-12">
                             <div class="course__tab-conent">
                                 <div class="tab-content" id="courseTabContent">
-                                    <div class="tab-pane fade show active" id="grid" role="tabpanel"
-                                         aria-labelledby="grid-tab">
+                                    <div class="tab-pane fade show active" id="grid" role="tabpanel" aria-labelledby="grid-tab">
 
-                                        <div class="row">
-                                            @foreach($companies as $company)
-                                                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 mb-4">
-                                                    <div class="card h-100">
-                                                        <a href="{{$company->link}}">
-                                                            <div class="course__thumb-2 w-img">
-                                                                @if($company->image)
-                                                                    <img src="{{$company->image_url}}"
-                                                                         class="card-img-top" alt="">
-                                                                @else
-                                                                    <img src="{{ asset("images/noImage2.webp") }}"
-                                                                         class="card-img-top" alt="">
-                                                                @endif
+                                        @if($companies->isEmpty())
+                                            <div class="text-center my-4">
+                                                <h5>Aradığınız kriterlere göre bir sonuç bulunamadı.</h5>
+                                            </div>
+                                        @else
+                                            <div class="row">
+                                                @foreach($companies as $company)
+                                                    <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 mb-4">
+                                                        <div class="card h-100">
+                                                            <a href="{{$company->link}}">
+                                                                <div class="course__thumb-2 w-img">
+                                                                    @if($company->image)
+                                                                        <img src="{{$company->image_url}}" class="card-img-top" alt="">
+                                                                    @else
+                                                                        <img src="{{ asset('images/noImage2.webp') }}" class="card-img-top" alt="">
+                                                                    @endif
+                                                                </div>
+                                                            </a>
+                                                            <div class="card-body d-flex flex-column">
+                                                                <div class="course__top-2 d-flex flex-wrap mb-2">
+                                                                    @if(count($company->courses) > 0)
+                                                                        @foreach($company->courses->take(5) as $course)
+                                                                            <div class="course__tag-2 mr-10 {{ \App\Service\Helper::randColor() }}">
+                                                                                <a>{{ strtoupper($course->name) }}</a>
+                                                                            </div>
+                                                                        @endforeach
+                                                                    @endif
+                                                                </div>
+                                                                <h3 class="mt-auto">
+                                                                    <a href="{{$company->link}}" class="stretched-link">
+                                                                        <span style="font-size: medium">{{$company->name}}</span>
+                                                                    </a>
+                                                                </h3>
                                                             </div>
-                                                        </a>
-                                                        <div class="card-body d-flex flex-column">
-                                                            <div class="course__top-2 d-flex flex-wrap mb-2">
-                                                                @if(count($company->courses) > 0)
-                                                                    @foreach($company->courses->take(5) as $course)
-                                                                        <div
-                                                                            class="course__tag-2 mr-10 {{ \App\Service\Helper::randColor() }}">
-                                                                            <a>{{ strtoupper($course->name) }}</a>
-                                                                        </div>
-                                                                    @endforeach
-                                                                @endif
-                                                            </div>
-                                                            <h3 class="mt-auto">
-                                                                <a href="{{$company->link}}"
-                                                                   class="stretched-link">
-                                                                        <span
-                                                                            style="font-size: medium">{{$company->name}}</span>
-                                                                </a>
-                                                            </h3>
                                                         </div>
                                                     </div>
-                                                </div>
-                                            @endforeach
-                                        </div>
+                                                @endforeach
+                                            </div>
+                                        @endif
+
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div class="row">
-                        <div class="col-xxl-12">
-                            <div class="basic-pagination">
-                                <nav>
-                                    <ul>
-                                        <!-- Previous Page Link -->
-                                        @if ($companies->onFirstPage())
-                                            <li class="disabled">
-                                                <span><i class="far fa-angle-left"></i></span>
-                                            </li>
-                                        @else
-                                            <li>
-                                                <a href="{{ $companies->previousPageUrl() }}">
-                                                    <i class="far fa-angle-left"></i>
-                                                </a>
-                                            </li>
-                                        @endif
-
-                                        <!-- Pagination Elements -->
-                                        @php
-                                            $start = max($companies->currentPage() - 2, 1);
-                                            $end = min($companies->currentPage() + 2, $companies->lastPage());
-                                        @endphp
-                                        @if ($start > 1)
-                                            <li>
-                                                <a href="{{ $companies->url(1) }}">1</a>
-                                            </li>
-                                            @if ($start > 2)
-                                                <li class="disabled"><span>...</span></li>
-                                            @endif
-                                        @endif
-
-                                        @foreach (range($start, $end) as $page)
-                                            @if ($page == $companies->currentPage())
-                                                <li>
-                                                    <span class="current">{{ $page }}</span>
+                    @if(!$companies->isEmpty())
+                        <div class="row">
+                            <div class="col-xxl-12">
+                                <div class="basic-pagination">
+                                    <nav>
+                                        <ul>
+                                            <!-- Previous Page Link -->
+                                            @if ($companies->onFirstPage())
+                                                <li class="disabled">
+                                                    <span><i class="far fa-angle-left"></i></span>
                                                 </li>
                                             @else
                                                 <li>
-                                                    <a href="{{ $companies->url($page) }}">{{ $page }}</a>
+                                                    <a href="{{ $companies->previousPageUrl() }}">
+                                                        <i class="far fa-angle-left"></i>
+                                                    </a>
                                                 </li>
                                             @endif
-                                        @endforeach
 
-                                        @if ($end < $companies->lastPage())
-                                            @if ($end < $companies->lastPage() - 1)
-                                                <li class="disabled"><span>...</span></li>
+                                            <!-- Pagination Elements -->
+                                            @php
+                                                $start = max($companies->currentPage() - 2, 1);
+                                                $end = min($companies->currentPage() + 2, $companies->lastPage());
+                                            @endphp
+                                            @if ($start > 1)
+                                                <li>
+                                                    <a href="{{ $companies->url(1) }}">1</a>
+                                                </li>
+                                                @if ($start > 2)
+                                                    <li class="disabled"><span>...</span></li>
+                                                @endif
                                             @endif
-                                            <li>
-                                                <a href="{{ $companies->url($companies->lastPage()) }}">{{ $companies->lastPage() }}</a>
-                                            </li>
-                                        @endif
 
-                                        <!-- Next Page Link -->
-                                        @if ($companies->hasMorePages())
-                                            <li>
-                                                <a href="{{ $companies->nextPageUrl() }}">
-                                                    <i class="far fa-angle-right"></i>
-                                                </a>
-                                            </li>
-                                        @else
-                                            <li class="disabled">
-                                                <span><i class="far fa-angle-right"></i></span>
-                                            </li>
-                                        @endif
-                                    </ul>
-                                </nav>
+                                            @foreach (range($start, $end) as $page)
+                                                @if ($page == $companies->currentPage())
+                                                    <li>
+                                                        <span class="current">{{ $page }}</span>
+                                                    </li>
+                                                @else
+                                                    <li>
+                                                        <a href="{{ $companies->url($page) }}">{{ $page }}</a>
+                                                    </li>
+                                                @endif
+                                            @endforeach
+
+                                            @if ($end < $companies->lastPage())
+                                                @if ($end < $companies->lastPage() - 1)
+                                                    <li class="disabled"><span>...</span></li>
+                                                @endif
+                                                <li>
+                                                    <a href="{{ $companies->url($companies->lastPage()) }}">{{ $companies->lastPage() }}</a>
+                                                </li>
+                                            @endif
+
+                                            <!-- Next Page Link -->
+                                            @if ($companies->hasMorePages())
+                                                <li>
+                                                    <a href="{{ $companies->nextPageUrl() }}">
+                                                        <i class="far fa-angle-right"></i>
+                                                    </a>
+                                                </li>
+                                            @else
+                                                <li class="disabled">
+                                                    <span><i class="far fa-angle-right"></i></span>
+                                                </li>
+                                            @endif
+                                        </ul>
+                                    </nav>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    @endif
 
                 </div>
 
